bf4f8805a3c50271b3c50a5391178074c43010f2
Bruce Vieira <bvieira@axway.com>
2024-07-11 12:05:18 -0300

    Adição de nova api, alteração nos fluxos de atualização e importação de api, criação de scripts em python e requirements.txt

---
+++ check-if-config-file-exists.py
@@ -0,0 +1,51 @@
+import os, json, docker
+import jsonpath_ng as jsonpath
+
+CONFIG_FILE = os.environ["CONFIG_FILE"]
+CONFIG_FILES = json.loads(CONFIG_FILE)
+APIM_IP = os.environ["APIM_INSTANCE_IP"]
+APIM_USER = os.environ["APIM_INSTANCE_USER"]
+APIM_PASSWORD = os.environ["APIM_INSTANCE_PASSWORD"]
+UPDATE_CONFIG_FILES = []
+IMPORT_CONFIG_FILES = []
+docker_client = docker.from_env()
+
+def export_env(key, value):
+    # Define a variável de ambiente no GITHUB_ENV para uso em steps subsequentes
+    with open(os.environ['GITHUB_ENV'], 'a', encoding='utf-8') as env_file:
+        env_file.write(f"{key}={value}\n")
+
+def export_env_and_output(key, value):
+    export_env(key, value)
+    # Escreve a saída no GITHUB_OUTPUT para uso no mesmo step
+    with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as output_file:
+        output_file.write(f"{key}={value}\n")
+
+def get_api_path(file_path):
+    print(f"{file_path}")
+    try:
+        with open(file_path, 'r') as file:
+            file_data = json.load(file)
+            name_query = jsonpath.parse("$.path")
+            json_field = name_query.find(file_data)
+            return json_field[0].value
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file {file_path}.")
+    except Exception as e:
+        print(f"An error occurred while processing the file {file_path}: {e}")
+
+for file in CONFIG_FILES:
+    path = get_api_path(file)
+    container_command = f"apim api get -h {APIM_IP} -u {APIM_USER} -port 8075 -p {APIM_PASSWORD} -a {path}"
+    print("Running api search in apim-cli...")
+    byte_result = docker_client.containers.run("bvieira123/apim-cli:1.14.4", container_command, stdout=True, stderr=True, remove=True)
+    result = byte_result.decode('utf-8')
+    if path in result:
+        UPDATE_CONFIG_FILES.append(file)
+    else:
+        IMPORT_CONFIG_FILES.append(file)
+
+export_env('UPDATE_CONFIG_FILES', UPDATE_CONFIG_FILES)
+export_env_and_output('MATRIX', json.dumps(IMPORT_CONFIG_FILES))
--- get-config-files.py
+++ get-config-files.py
@@ -1,19 +1,53 @@
-[Yesterday 6:07 PM] Joao Jacinto de Barros (External)
-export_env_and_output('MATRIX', json.dumps(filtered_environment_names))
- 
-[Yesterday 6:07 PM] Joao Jacinto de Barros (External)
-def export_env_and_output(key, value):
-    # Define a variável de ambiente no GITHUB_ENV para uso em steps subsequentes
-    with open(os.environ['GITHUB_ENV'], 'a', encoding='utf-8') as env_file:
-        env_file.write(f"{key}={value}\n")
-   
-    # Escreve a saída no GITHUB_OUTPUT para uso no mesmo step
-    with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as output_file:
-        output_file.write(f"{key}={value}\n")
- 
-[Yesterday 6:09 PM] Joao Jacinto de Barros (External)
-    strategy:
-      matrix:
-        environment: ${{fromJson(needs.pre-processamento.outputs.matrix)}}
-    environment: ${{ matrix.environment }}
- 
+import json, os
+import jsonpath_ng as jsonpath
+
+file_list = os.environ["CHANGE"].split(',')
+conf_files_list = []
+
+def add_in_conf_files_list(file):
+    if file not in file_list:
+        conf_files_list.append(config_file)
+
+def export_env(key, value):
+  # Define a variável de ambiente no GITHUB_ENV para uso em steps subsequentes
+  with open(os.environ['GITHUB_ENV'], 'a', encoding='utf-8') as env_file:
+      env_file.write(f"{key}={value}\n")
+
+def get_config_file_of_api_definition_file(file_path, file_name):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            name_query = jsonpath.parse("$.apiSpecification.resource")
+            value = name_query.find(data)
+            if file_name in value[0].value:
+                return file_path
+            return ""
+            
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file {file_path}.")
+    except Exception as e:
+        print(f"An error occurred while processing the file {file_path}: {e}")
+
+def process_api_defition(file_path):
+    directory, file_name = os.path.split(file_path)
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('-config.json'):
+                file_path = os.path.join(root, file)
+                config_file_path = get_config_file_of_api_definition_file(file_path, file_name)
+                if config_file_path != "":
+                    return config_file_path
+
+for file in file_list:
+    config_file = ""
+    if "-config" in file.lower():
+      print(f"Processing config file: {file}")
+      conf_files_list.append(file)
+    else:
+        print(f"Processing API Definition file")    
+        config_file = process_api_defition(file)
+        add_in_conf_files_list(config_file)
+
+export_env('CONFIG_FILES', json.dumps(conf_files_list))
